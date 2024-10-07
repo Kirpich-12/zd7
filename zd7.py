32161558885815eecb9d2e1e60e9f583e5932a43
Kirpich-12 <ponchik34@tuta.io>
2024-10-07 12:23:02 +0300

    frs

--- zd7.py
+++ zd7.py
@@ -2,7 +2,7 @@ input_data = open('input.txt', 'r')
 data = input_data.read()
 data = data.split()
 
-ans = str(max(int(data[0]), int(data[1]), int(data[2])))
+ans = str(max(int(data[0]), int(data[1]), int(data[2]))) #выбор максимального
 
 output_data = open('output.txt', 'w')
 output_data.write(ans)
